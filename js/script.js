869515925f1c645bd4515bb35707ebd345c7b245
leepeggy0528 <peggy6088@gmail.com>
2021-10-31 02:22:59 +0800

    RWD-header

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ $(function(){
         }
     });
 });
+
+function openCloseMenu() {
+    document.querySelector(".hamburger .menuHB").classList.toggle("open");
+}
+function CloseMenu() {
+    document.querySelector(".hamburger .menuHB").classList.toggle("close");
+}
+
+function init() {
+    if (document.querySelector(".hamburger .open")==true) {
+        document.getElementById("body").onclick=CloseMenu;
+    }else{
+        document.getElementById("btnHamburger").onclick=openCloseMenu;
+    }
+}
+window.addEventListener("load",init,false);
