6cd4a9ff2674dbd0bb26cbcbb44548f5c52815b8
leepeggy0528 <peggy6088@gmail.com>
2021-11-01 04:13:21 +0800

    js

--- js/script.js
+++ js/script.js
@@ -38,3 +38,21 @@ window.addEventListener("load",init,false);
 $("#top").click (function(){
     $("html,body").animate({scrollTop:0 },500);
 });
+
+window.addEventListener("load",function () {
+    let text=document.getElementById('count').value;
+    document.getElementById('plus').addEventListener("click",function (e) {
+        text=parseInt(text)+1;
+        document.getElementById('count').value=text;
+        e.stopPropagation()
+    },false);
+    if ( parseInt(document.getElementById('count').value)<=0) {
+        document.getElementById('minus').disable=true;
+        document.getElementById('minus').style.opacity='0.5';
+    }else{
+        document.getElementById('minus').addEventListener("click",function () {
+            text=parseInt(text)-1;
+            document.getElementById('count').value=text;
+        },false);
+    }   
+},false);
